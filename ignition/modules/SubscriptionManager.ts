901cf21872ca330f1ec1762b48175acd8f446555
Mikefrfr <mohitsusangeorge@gmail.com>
2025-09-19 15:08:19 +0530

    hrishikesh's securepay frontend

--- ignition/modules/SubscriptionManager.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const SubscriptionManagerModule = buildModule("SubscriptionManagerModule", (m) => {
-  const subManager = m.contract("SubscriptionManager");
-  return { subManager };
-});
-
-export default SubscriptionManagerModule;
